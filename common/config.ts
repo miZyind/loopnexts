959cb00641bef02e116462a3a04bc8ff9b635bd4
miZyind <mizyind@gmail.com>
2018-12-26 16:09:24 +0700

    refine project config

--- common/config.ts
+++ common/config.ts
@@ -1,14 +1,68 @@
+// Dotenv
+import 'dotenv/config';
+// Node module
+import path from 'path';
+// tslint:disable-next-line:ordered-imports
+import withCSS from '@zeit/next-css';
+import WithTypescript from '@zeit/next-typescript';
+import withOptimizedImages from 'next-optimized-images';
 import { ApplicationConfig } from '@loopback/core';
+// Definition
+import { ServerOptions } from 'next-server';
 
-const config: ApplicationConfig = {
+const isDev = process.env.NODE_ENV !== 'production';
+const {
+  NAME: name,
+  VERSION: version,
+  PROTOCOL: protocol,
+  HOST: host,
+  PORT: port,
+  BASE_PATH: basePath,
+} = process.env;
+
+const loopbackConfig: ApplicationConfig = {
   rest: {
-    port: +process.env.PORT! || 3000,
-    host: process.env.HOST || 'localhost',
+    port,
+    host,
+    protocol,
+    basePath,
     openApiSpec: {
-      // useful when used with OASGraph to locate your application
       setServersFromRequest: true,
     },
   },
 };
 
-export default config;
+const nextConfig: ServerOptions = {
+  dev: isDev,
+  dir: path.resolve(process.cwd(), 'client'),
+  quiet: true,
+  conf: withCSS(
+    WithTypescript(
+      withOptimizedImages({
+        publicRuntimeConfig: { name, version },
+        webpack(config) {
+          config.module!.rules.push({
+            test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/i,
+            use: {
+              loader: 'file-loader',
+              options: {
+                limit: 8192,
+                publicPath: './',
+                outputPath: 'static/css/',
+                name: '[name].[ext]',
+              },
+            },
+          });
+
+          return config;
+        },
+      }),
+    ),
+  ),
+};
+
+export {
+  isDev,
+  loopbackConfig,
+  nextConfig,
+};

26f995bcbb8fcd32bef75d0d4ee233bf74e7a1da
miZyind <mizyind@gmail.com>
2019-04-11 01:14:00 +0700

    update typescript settings and packages

--- typings/app.d.ts
+++ typings/app.d.ts
@@ -3,3 +3,7 @@ declare module 'next-fonts';
 declare module '@zeit/next-css';
 declare module 'next-optimized-images';
 declare module '@zeit/next-typescript';
+// Redux DevTool
+interface Window {
+  observer: any;
+}

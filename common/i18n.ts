ffc8cdfb18f5663d137f99b1db13194bd85d00e7
miZyind <mizyind@gmail.com>
2019-05-16 10:51:04 +0700

    refine i18n settings

--- common/i18n.ts
+++ common/i18n.ts
@@ -1,23 +1,34 @@
 // Node module
-import i18next from 'i18next';
+import i18n from 'i18next';
 import XHR from 'i18next-xhr-backend';
 import BrowserLanguageDetector from 'i18next-browser-languagedetector';
 
-const options: i18next.InitOptions = {
+const options: i18n.InitOptions = {
+  debug: false,
   fallbackLng: 'en',
+  preload: ['en'],
   load: 'languageOnly',
-  ns: 'common',
+  ns: ['common'],
   defaultNS: 'common',
   interpolation: {
-    escapeValue: false, // react already safes from xss
+    escapeValue: false,
+    formatSeparator: ',',
+    format: (value, format) =>
+      format === 'uppercase' ? value.toUpperCase() : value,
+  },
+  backend: {
+    loadPath: 'static/locales/{{lng}}/{{ns}}.json',
+    ignoreRoutes: ['/_next'],
   },
 };
 
-if (!i18next.isInitialized) {
-  i18next
-    .use(XHR)
-    .use(BrowserLanguageDetector)
-    .init(options);
+if (!i18n.isInitialized) {
+  if (process.browser) {
+    i18n.use(XHR).use(BrowserLanguageDetector);
+  } else {
+    // TODO: Backend
+  }
+  i18n.init(options);
 }
 
-export default i18next;
+export default i18n;

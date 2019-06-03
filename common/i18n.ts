75af0896e08c59efa50468d0b390899605f3d04c
miZyind <mizyind@gmail.com>
2019-06-03 11:58:00 +0700

    refine i18n settings

--- common/i18n.ts
+++ common/i18n.ts
@@ -2,7 +2,8 @@
 import i18n from 'i18next';
 import { resolve } from 'path';
 import XHR from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import FrontendLngDetector from 'i18next-browser-languagedetector';
+import { LanguageDetector as BackendLngDetector } from 'i18next-express-middleware';
 
 const isBrowser = process.browser;
 const localePath = 'static/locales/{{lng}}/{{ns}}.json';
@@ -30,12 +31,20 @@ const options: i18n.InitOptions = {
     lookupQuerystring: 'lng',
     caches: ['cookie'],
   },
+  react: {
+    wait: true,
+  },
 };
 
-i18n
-  .use(isBrowser ? XHR : eval(`require('i18next-node-fs-backend')`))
-  .use(LanguageDetector)
-  .init(options);
+if (isBrowser) {
+  i18n.use(XHR).use(FrontendLngDetector);
+} else {
+  i18n.use(eval(`require('i18next-node-fs-backend')`)).use(BackendLngDetector);
+}
+
+if (!i18n.isInitialized) {
+  i18n.init(options);
+}
 
 export type I18n = typeof i18n;
 export type Language = typeof i18n.language;

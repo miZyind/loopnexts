86805399c40bb880a13d5585c5c7ccd62e10266d
miZyind <mizyind@gmail.com>
2019-06-25 02:22:46 +0700

    apply react immer hook reducer context helper

--- client/contexts/ui.tsx
+++ client/contexts/ui.tsx
@@ -1,28 +1,20 @@
 // Node module
-import produce from 'immer';
-import { FC, createContext, useReducer, useContext } from 'react';
-import { ActionType, createStandardAction } from 'typesafe-actions';
-
-import {
-  IActionCreators,
-  IMappedActionCreators,
-  mapDispatchToActionCreators,
-} from '../helpers';
+import { createAction, createContext } from '../helpers';
 
 /**
- * The Standard Bootstrap 4.x Breakpoints
- *  https://medium.com/@uiuxlab/the-most-used-responsive-breakpoints-in-2017-of-mine-9588e9bd3a8a
+ * The Standard Semantic UI Breakpoints
+ * https://semantic-ui.com/elements/container.html
+ * https://medium.com/@uiuxlab/the-most-used-responsive-breakpoints-in-2017-of-mine-9588e9bd3a8a
  */
-enum Breakpoints {
-  mobile = 575,
-  tablet = 991,
-  desktop = 1199,
-}
-
 enum DisplayTypes {
-  mobile = 'mobile',
-  tablet = 'tablet',
-  desktop = 'desktop',
+  // > 1200
+  LARGE_MONITOR = 'large monitor',
+  // 992 ~ 1200
+  SMALL_MONITOR = 'small monitor',
+  // 768 ~ 991
+  TABLET = 'tablet',
+  // < 768
+  MOBILE = 'mobile',
 }
 
 interface IState {
@@ -30,52 +22,40 @@ interface IState {
 }
 
 const initialState: IState = {
-  displayType: DisplayTypes.mobile,
+  displayType: DisplayTypes.MOBILE,
 };
 
 enum ActionTypes {
-  windowResize = '[ui] window resize',
+  WINDOW_RESIZE = '[ui] window resize',
 }
 
 const actionCreators = {
-  windowResize: createStandardAction(ActionTypes.windowResize)<number>(),
+  windowResize: (width: number) =>
+    createAction(ActionTypes.WINDOW_RESIZE, width),
 };
 
-type Action = ActionType<typeof actionCreators>;
-
-const reducer = (state = initialState, action: Action) =>
-  produce(state, (draft) => {
+const { ContextProvider, useContext } = createContext(
+  initialState,
+  actionCreators,
+  (draft, action) => {
     switch (action.type) {
-      case ActionTypes.windowResize:
+      case ActionTypes.WINDOW_RESIZE:
         const { payload: width } = action;
-        draft.displayType = DisplayTypes.desktop;
-        if (width > Breakpoints.mobile && width <= Breakpoints.tablet) {
-          draft.displayType = DisplayTypes.tablet;
+        if (width > 1200) {
+          draft.displayType = DisplayTypes.LARGE_MONITOR;
         }
-        if (width <= Breakpoints.mobile) {
-          draft.displayType = DisplayTypes.mobile;
+        if (width >= 992 && width <= 1200) {
+          draft.displayType = DisplayTypes.SMALL_MONITOR;
+        }
+        if (width >= 768 && width <= 991) {
+          draft.displayType = DisplayTypes.TABLET;
+        }
+        if (width < 768) {
+          draft.displayType = DisplayTypes.MOBILE;
         }
         break;
     }
-  });
+  },
+);
 
-interface IContext {
-  state: IState;
-  actions?: IMappedActionCreators<Action, IActionCreators<Action>>;
-}
-
-const UIContext = createContext<IContext>({
-  state: initialState,
-});
-
-export const UIContextProvider: FC = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const actions = mapDispatchToActionCreators(actionCreators, dispatch);
-  return (
-    <UIContext.Provider value={{ state, actions }}>
-      {children}
-    </UIContext.Provider>
-  );
-};
-
-export const useUIContext = () => useContext(UIContext);
+export { ContextProvider as UIContextProvider, useContext as useUIContext };
